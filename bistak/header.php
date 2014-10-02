7f60cfc483a69de56de02bc2844cbee348a2b297
esars <substrace@opmbx.org>
2014-10-02 12:42:50 +0200

    beharrezko liburutegiak gehitu

--- bistak/header.php
+++ bistak/header.php
@@ -3,8 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Morea Lorategia</title>
+	<link rel="stylesheet" type="text/css" href="public/style.css">
+	<link rel="stylesheet" type="text/css" href="public/pure.css">
+	<script src="public/jquery.js"></script>
 </head>
 <body>
 	<header>
-		<h1>Morea Lorategia</h1>
+		<img src="public/img/logoa.png" alt="Logoa" id="logoa">
 	</header>
+	<nav>
+		<ul>
+			<li>Produktuak</li>
+			<li>Gu</li>
+			<li>Kokapena</li>
+		</ul>
+	</nav>
