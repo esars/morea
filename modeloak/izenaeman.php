fb642431f7cfeec68b1a003b2ab3de65caa8ad41
esars <substrace@opmbx.org>
2014-10-03 12:34:52 +0200

    misc

--- modeloak/izenaeman.php
+++ modeloak/izenaeman.php
@@ -2,10 +2,10 @@
 
 class IzenaEman {
 		private $db = null;
-	
+
 		public $erroreak = array();
 		public $mezuak = array();
-		
+
 		public function __construct() {
 				if(isset($_POST['izenaeman'])) {
 						$this->erregistratu();
@@ -13,8 +13,11 @@ class IzenaEman {
 						$this->erroreak[] = "Errore ezezaguna";
 				}
 		}
-		
+
 		private function erregistratu() {
+
+				//	MINIMOAK ETA KARAKTERE BALIOZKOAK	//
+
 				if(empty($_POST['izena'])) {
 						$this->erroreak = "Izena hutsik zegoen";
 				} else if(empty($_POST['pasahitza']) OR empty($_POST['pasahitza1'])) {
@@ -29,51 +32,51 @@ class IzenaEman {
 						$this->erroreak = "Emaila ezin da hutsik egon";
 				} else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 						$this->erroreak = "Emailaren sintaxia ez da egokia";
-				} 
-				
+				}
+
 				//	Gauza gehio gehitu behar dira, helbidea, telefonoa... dena luzera minimo baten
-				
+
 				else {
 						//	DATUAK SARTU	//
-						
-						$this->db = new mysqli_connect($config["host"], 
+
+						$this->db = new mysqli_connect($config["host"],
 																					 $config["user"],
 																					 $config["pass"],
 																					 $config["izen"]);
 						if(!$this->db->connect_errno) {
-								
+
 								/*
 								 * real_escape_string eta strip_tags HTML eta JS
 								 * 'garbitu' egiten ditu kodigoa testu normal
 								 * bihurtuz
 								*/
-								
+
 								$izena = $this->db->real_escape_string(strip_tags($_POST['izena'], ENT_QUOTES));
 								$email = $this->db->real_escape_string(strip_tags($_POST['email'], ENT_QUOTES));
 								$helbi = $this->db->real_escape_string(strip_tags($_POST['helbidea'], ENT_QUOTES));
 								$pass = $_POST['pasahitza'];
 								$telf = $_POST['telefonoa'];
-		
+
 								//	password_hash PHP 5.3tik aurrera dabil	//
-								
+
 								$pass_hash = password_hash($pass, PASSWORD_DEFAULT);
-								
+
 								//	Hemen salt egitea falta da, ez nago guztiz ziur goiko funtzioa erabilita beharrezkoa den	//
-								
+
 								//	Emaila erabilpenean dagoen konprobatu	//
-								
+
 								$sql = "SELECT * FROM erabiltzaile WHERE email='".$email."';";
 								$emailaErabilpenean = $this->db->query($sql);
-								
+
 								if($emailaErabilpenean->num_rows == 1) {
 										$this->erroreak[] = "Emaila erabilpenean dago";
 								} else {
 										$sql = "INSERT INTO erabiltzaile
 														(izena, email, helbidea, pasahitza, telefonoa)
-														VALUES 
+														VALUES
 														(".$izena.", ".$email.", ".$helbi.", ".$pass.", ".$telf.");";
 										$inserzioa = $this->db->query($sql);
-										
+
 										//	Datuak sartu diren ala ez konprobatu	//
 										
 										if($inserzioa) {
@@ -81,7 +84,7 @@ class IzenaEman {
 										} else {
 											$this->erroreak[] = "Errorea izena ematean. Saia zaitez berriro"
 										}
-										
+
 								}
 						} else {
 								$this->erroreak[] = "Errorea datubasera konektatzean";
