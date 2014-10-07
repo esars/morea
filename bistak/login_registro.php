cb0131681a8e05db9b550414ead02bc1d99e3ca2
xaxtian <xaxtian.amenabar@gmail.com>
2014-10-07 13:04:20 +0200

    formak zentratuak

--- bistak/login_registro.php
+++ bistak/login_registro.php
@@ -1,7 +1,8 @@
 <div id="ezkutatua">
 <div id="mylightbox">
+	<div id="regi">
 <form class='pure-form' action="" method='post'>
-	<h1>Login</h1>
+	<h1>Sartu</h1>
 	<fieldset class='pure-group'>
 	<input class='pure-input-1-6' type="text" maxlength='40' required value='Emaila' name='email'>
 	<input class='pure-input-1-6' type="text" maxlength='70' required value='Pasahitza' name='pasahitza'>
@@ -9,14 +10,14 @@
 	<input class='pure-button pure-input-1-6 pure-button-primary' type="submit" value='Sartu'>
 </form>
 <form id='registro' class='pure-form' action="" method='post'>
-	<h1>Registrarse</h1>
+	<h1>Erregistratu</h1>
 	<fieldset class='pure-group'>
 	<input class='pure-input-1-6' type="text" required maxlength='30' value='Izena' name='izena'>
 	<input class='pure-input-1-6' type="text" required maxlength='30' value='Abizena' name='abizena'>
 	<input class='pure-input-1-6' type="email" required maxlength='40' value='Emaila' name='email'>
 	</fieldset>
 	<fieldset class='pure-group'>
-	<input class='pure-input-1-6' type="number" required max='12' value='Telefonoa' name='telefono'>
+	<input class='pure-input-1-6' type="number" required max='999999999999' value='Telefonoa' name='telefono'>
 	<input class='pure-input-1-6' type="text" required maxlength='70' value='Helbidea' name='helbidea'>
 	</fieldset>
 	<fieldset class='pure-group'>
@@ -25,4 +26,5 @@
 	</fieldset>
 	<input type="submit" class='pure-button pure-input-1-6 pure-button-primary' id='registro1' value='Erregistratu'>
 </form>
+</div>
 </div></div>
