76ba100dd3093e4d829b26462e0343ff70afacdb
xaxtian <xaxtian.amenabar@gmail.com>
2014-10-09 14:46:49 +0200

    formen submit botoiak eta saskia

--- bistak/login_registro.php
+++ bistak/login_registro.php
@@ -1,14 +1,6 @@
 <div id="ezkutatua">
 <div id="mylightbox">
 	<div id="regi">
-<form class='pure-form' action="" method='post'>
-	<h1>Sartu</h1>
-	<fieldset class='pure-group'>
-	<input class='pure-input-1-6' type="text" maxlength='40' required value='Emaila/Izena' name='email'>
-	<input class='pure-input-1-6' type="password" maxlength='70' required value='Pasahitza' name='pasahitza'>
-	</fieldset>
-	<input class='pure-button pure-input-1-6 pure-button-primary formbotoi' type="submit" name="login" value='Sartu'>
-</form>
 <form id='registro' class='pure-form' action="" method='post'>
 	<h1>Erregistratu</h1>
 	<fieldset class='pure-group'>
@@ -25,6 +17,14 @@
 	<input class='pure-input-1-6' type="password" maxlength='70' required value='Errepikatu Pasahitza' name='pasahitza2'>
 	</fieldset>
 	<input type="submit" class='pure-button pure-input-1-6 pure-button-primary formbotoi' id='registro1' value='Erregistratu' name="izenaeman">
+</form>
+<form class='pure-form' action="" method='post'>
+	<h1>Sartu</h1>
+	<fieldset class='pure-group'>
+	<input class='pure-input-1-6' type="text" maxlength='40' required value='Emaila/Izena' name='email'>
+	<input class='pure-input-1-6' type="password" maxlength='70' required value='Pasahitza' name='pasahitza'>
+	</fieldset>
+	<input class='pure-button pure-input-1-6 pure-button-primary formbotoi' type="submit" name="login" value='Sartu'>
 </form>
 </div></div>
 </div>
---
+++ modeloak/saskia.php
@@ -0,0 +1,86 @@
+<?php
+class Saskia{
+	private $db=null;
+
+	public $erroreak = [];
+	public $mezuak = [];
+
+		/*
+		 * Hemen CRUD funtzionalitate basikoa egongo da programatuta.
+		 *        ^
+		 * Create Read Update Delete
+		 * 
+		 * Datubasera queryak egiten dituen edozein funtziok pribatua
+		 * izan behar du, eta kontrolatzailearen bidez abiaraziko degu.
+		*/
+		
+		public function __construct() {
+			
+				/*
+				 * Produktu->handle() Kontrolatzailea abiarazi.
+				 * Web orrialdearen konplexutasuna haundituko balitz
+				 * kontrolatzaile bat baino gehiago inplementatu daiteke.
+				*/
+				
+				if(isset($_GET['karrito'])) {
+					$this->handle($_GET["karrito"]);
+				}
+		}
+		
+		private function handle($karrito = null) {
+			
+			/* PRODUKTUEN KONTROLATZAILEA
+			 * 
+			 * GETaren arabera funtzio bat edo bestea egingo du.
+			 * Hemen ere datubasera konexioa egiten dugu. Funtzionalitate
+			 * orokor gehiago gehitu daitezke.
+			 * 
+			*/
+			
+			global $config;
+			
+			$this->db = mysqli_connect($config["host"],
+															 $config["user"],
+															 $config["pass"],
+															 $config["izen"])
+									or die("Error " . mysqli_error($this->db));
+			
+			switch($karrito) {
+				case "gehitu":
+					$this->saskiraGehitu();
+					break;
+				case "kendu":
+					$this->saskitikKendu();
+					break;
+			}
+			$this->saskiaErakutsi();
+		}
+		private function saskiraGehitu() {
+			//kontadorea ez bada existitzen 0 balioarekin sortzen dugu
+				if(!isset($_SESSION['contador'])){
+					$_SESSION['contador']=0;
+				}
+				if(isset($_POST["produktua"])) {
+			//karritoaren array-ean kontadorea momentu horretan
+			// dagoen posizioan sartuko dugu produktuaren id-a
+					$_SESSION['karritoa'][$_SESSION['contador']]=$_POST['produktua'];
+			//Urrengo produktua array-ean urrengo posizioan gehitzeko contadorea +1 egiten dugu
+					$_SESSION['contador']++;
+				}
+		}
+		private function saskitikKendu() {
+				if(isset($_POST["ezabatzekoak"])) {
+			//Ezabatzekoak postak ezabatzeko produktuen id-ak zerrenda moduan ekarriko ditu ',' banatuak
+					$ezabatzeko_array=explode(',',$_POST["ezabatzekoak"]);
+					$max=sizeOf($ezabatzeko_array);
+					for($i=0;$i<$max;$i++){
+						$kendu=$ezabatzeko_array[$i];
+						$pos=array_search($kendu,$_SESSION['karritoa']);
+						unset($_SESSION['karritoa'][$pos]);
+					}
+				}
+		}
+		private function saskiaErakutsi() {
+				
+		}
+}
