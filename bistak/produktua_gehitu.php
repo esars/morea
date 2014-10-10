13fc77da2ea67d233c3b844b40e39e639918c398
xaxtian <xaxtian.amenabar@gmail.com>
2014-10-10 10:02:46 +0200

    text area width

--- bistak/produktua_gehitu.php
+++ bistak/produktua_gehitu.php
@@ -1,25 +1,10 @@
 <div class="gehituform">
-  <form class="pure-form pure-form-aligned" method="post" action="">
-      <fieldset>
-          <div class="pure-control-group">
-              <label for="pizena">Produktuaren izena</label>
-              <input id="pizena" name="pizena" required type="text" placeholder="Larrosa erramua">
-          </div>
-
-          <div class="pure-control-group">
-              <label for="deskripzioa">Deskripzioa</label>
-              <textarea id="deskripzioa" name="deskripzioa" required placeholder="Deskripzio bat gehitu zure produktuari..."></textarea>
-          </div>
-
-          <div class="pure-control-group">
-              <label for="prezioa">Prezioa</label>
-              <input id="prezioa" required type="number" placeholder="Prezio bat ezarri" name="prezioa">
-          </div>
-
-          <div class="pure-control-group">
-              <label for="foo">Stock</label>
-              <input id="foo" type="number" name="stock" placeholder="Sartu eskura dauden unitateak...">
-          </div>
+ <form class='pure-form' action="" method='post'>
+      <fieldset class='pure-group'>
+              <input class='pure-input-1-6' id="pizena" name="pizena" required type="text" placeholder="Produktuaren izena">
+              <textarea class='pure-input-1-6' id="deskripzioa" name="deskripzioa" required placeholder="Deskripzioa"></textarea>
+              <input class='pure-input-1-6' id="prezioa" required type="number" placeholder="Prezioa" name="prezioa">
+              <input class='pure-input-1-6' id="foo" type="number" name="stock" placeholder="Unitateak">
       </fieldset>
       <input type="submit" class='pure-button pure-input-1-6 pure-button-primary formbotoi' id='registro1' value='Produktua datubasera gehitu' name="pgehitu">
   </form>
